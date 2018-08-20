e88923fb24701e9ec142dc28dd6ae07169c01026
Weihs, Godfried <godfried.weihs@aruplab.com>
2018-08-20 11:49:25 -0600

    Add project files.

---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesAndEvents
+{
+    public delegate void WorkPerformedHandler(int hours, WorkType workType);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
+            WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
+
+            // These are hard coded
+            del1(5, WorkType.Golf);
+            del2(10, WorkType.GenerateReports);
+
+            // This provides an example of a dynamic delegate call
+            DoWork(del1);
+            DoWork(del2);
+        }
+
+        static void DoWork(WorkPerformedHandler del)
+        {
+            del(12, WorkType.GoToMeetings);
+        }
+
+        static void WorkPerformed1(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed1 called " + hours.ToString());
+        }
+
+        static void WorkPerformed2(int hours, WorkType workType)
+        {
+            Console.WriteLine("WorkPerformed2 called " + hours.ToString());
+        }
+    }
+    public enum WorkType
+    {
+        GoToMeetings,
+        Golf,
+        GenerateReports
+    }
+}
