c37148c98f242b5813f1a5473d27ddc308572b00
Weihs, Godfried <godfried.weihs@aruplab.com>
2018-08-21 10:39:10 -0600

    Using Lambdas with custom Delegates

--- DelegatesAndEvents/Program.cs
+++ DelegatesAndEvents/Program.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DelegatesAndEvents
 {
+    public delegate int BizRulesDelegate(int x, int y);
     class Program
     {
         static void Main(string[] args)
         {
+            BizRulesDelegate addDel = (x, y) => x + y;
+            BizRulesDelegate multiplyDel = (x, y) => x * y;
+
+            var data = new ProcessData();
+            data.Process(2, 3, addDel);
+            data.Process(2, 3, multiplyDel);
+
+            var worker = new Worker();
+
             //WorkPerformedHandler del1 = new WorkPerformedHandler(WorkPerformed1);
             //WorkPerformedHandler del2 = new WorkPerformedHandler(WorkPerformed2);
             //WorkPerformedHandler del3 = new WorkPerformedHandler(WorkPerformed3);
@@ -27,21 +38,22 @@ namespace DelegatesAndEvents
             //int finalHours = del1(10, WorkType.GenerateReports);// The last delegate in the invocation list is what will get returned
             //Console.WriteLine(finalHours);
 
-            var worker = new Worker();
             // This is the manual way of hooking up the event handlers
             //worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
             //worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
 
             // This is how its done using Delegate Inference
             //worker.WorkPerformed += Worker_WorkPerformed; // The compiler already knows how the events and they types are set up in the Worker class,
-            worker.WorkCompleted += Worker_WorkCompleted; // so we don't need all the extra code
+            //worker.WorkCompleted += Worker_WorkCompleted; // so we don't need all the extra code
 
             // This is how its done using an Anonymous Method.
-            worker.WorkPerformed += delegate (object sender, WorkPerformedEventArgs e)
-            {
-                Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
-            };
+            //worker.WorkPerformed += delegate (object sender, WorkPerformedEventArgs e)
+            //{ Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType); };
+            //worker.WorkCompleted += delegate(object sender, EventArgs e) { Console.WriteLine("Worker is done"); };
 
+            // Using Lambdas
+            worker.WorkPerformed += (s, e) => Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+            worker.WorkCompleted += (s, e) => Console.WriteLine("Worker is complete");
             worker.DoWork(8, WorkType.GenerateReports);
         }
 
@@ -50,10 +62,10 @@ namespace DelegatesAndEvents
         //    Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
         //}
 
-        static void Worker_WorkCompleted(object sender, EventArgs e)
-        {
-            Console.WriteLine("Worker is done");
-        }
+        //static void Worker_WorkCompleted(object sender, EventArgs e)
+        //{
+        //    Console.WriteLine("Worker is done");
+        //}
 
         //static void DoWork(WorkPerformedHandler del)
         //{
